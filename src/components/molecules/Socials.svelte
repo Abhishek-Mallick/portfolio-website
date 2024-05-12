89f011af6ea4da39cadf47d0e43276def2a4beef
Abhishek Mallick <mallickabhishek97@gmail.com>
2024-05-12 19:53:36 +0530

    fix: Updated server logic

--- src/components/molecules/Socials.svelte
+++ src/components/molecules/Socials.svelte
@@ -1,16 +1,46 @@
 <script>
-	import Social from '../atoms/Social.svelte';
-</script>
+	import Social from "../atoms/Social.svelte";
 
-<div class="socials-container">
-	<Social icon="github" tip="Abhishek-Mallick" link="https://github.com/Abhishek-Mallick" />
-	<Social icon="discord" tip="abhishek-mallick09" link={`https://www.linkedin.com/in/abhishek-mallick09/`} />
-	<Social icon="email" tip="mallickabhishek97@gmail.com" link="mailto:mallickabhishek97@gmail.com" />
-</div>
-
-<style>
-	.socials-container {
-		display: flex;
-		gap: 0.75rem;
+	function copyToClipboard(text) {
+	  try {
+		navigator.clipboard.writeText(text);
+		alert("Email copied to clipboard!");
+	  } catch {
+		const element = document.createElement('textarea');
+		const previouslyFocusedElement = document.activeElement;
+  
+		element.value = text;
+  
+		element.setAttribute('readonly', '');
+		element.style.position = 'absolute';
+		element.style.left = '-9999px';
+  
+		document.body.appendChild(element);
+		element.select();
+		document.execCommand('copy');
+		document.body.removeChild(element);
+  
+		if (previouslyFocusedElement) {
+		  previouslyFocusedElement.focus();
+		}
+		alert("Email copied to clipboard!");
+	  }
 	}
-</style>
+  </script>
+  
+  <div class="socials-container">
+	  <Social icon="github" tip="Abhishek-Mallick" link="https://github.com/Abhishek-Mallick" />
+	  <Social icon="discord" tip="abhishek-mallick09" link={`https://www.linkedin.com/in/abhishek-mallick09/`} />
+	  
+	  <a href="mailto:mallickabhishek97@gmail.com" on:click|preventDefault={() => copyToClipboard('mallickabhishek97@gmail.com')}>
+	  <Social icon="email" tip="mallickabhishek97@gmail.com" />
+	</a>
+  </div>
+  
+  <style>
+	  .socials-container {
+		  display: flex;
+		  gap: 0.75rem;
+	  }
+  </style>
+  
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -63,7 +63,7 @@
 	<meta name="twitter:image" itemprop="image" content="/x00.png" />
 	<meta name="twitter:card" content="summary" />
 	<meta name="theme-color" content="#CCE2F2" />
-	<title>Portfolio</title>
+	<title>Abhishek Mallick</title>
 </svelte:head>
 
 <svelte:window on:click={playSFX} />
