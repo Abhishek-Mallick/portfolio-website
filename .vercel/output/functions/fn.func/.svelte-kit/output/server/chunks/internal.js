ce6ffeeba2c701e909470c9b0517852ea1a00437
Abhishek-Mallick <mallickabhishek97@gmail.com>
2026-03-06 02:49:41 +0530

    fix

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/chunks/internal.js
@@ -185,7 +185,7 @@ const options = {
 		<div class="error">
 			<span class="status">` + status + '</span>\n			<div class="message">\n				<h1>' + message + "</h1>\n			</div>\n		</div>\n	</body>\n</html>\n"
   },
-  version_hash: "17wg7wy"
+  version_hash: "1fg9ln5"
 };
 function get_hooks() {
   return {};
