cb626ce485209fd3f703f0aad2d4bc855b866e46
Abhishek-Mallick <mallickabhishek97@gmail.com>
2026-03-06 02:32:21 +0530

    fix: deployment

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.ec5fd274.js","_app/immutable/chunks/scheduler.09a1dfe1.js","_app/immutable/chunks/index.b880ee5b.js","_app/immutable/chunks/stores.524bab40.js","_app/immutable/chunks/singletons.52bd8753.js"];
-export const stylesheets = ["_app/immutable/assets/1.cf8ae120.css"];
-export const fonts = [];
